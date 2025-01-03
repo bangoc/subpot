737b58a201dbb65732f6ce8e167a7a8ebcf39432
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2025-01-03 23:25:35 +0700

    Config override and default

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
 const { app, dialog, BrowserWindow, Menu, MenuItem } = require('electron/main')
+const fs = require("fs");
 const path = require('node:path')
+
+const app_path = app.getAppPath()
+const config_override = path.join(app_path, "config-override")
+const local_default_config = path.join(app_path, "config-default.json")
+
+var config;
+
+async function loadConfig() {
+  config_url = 'https://bangoc.github.io/subpot/config.json'
+  console.log("check: " + config_override)
+  if (fs.existsSync(config_override)) {
+    const contents = fs.readFileSync(config_override, 'utf-8')
+    config_url = contents.split(/\r?\n/)[0]
+    console.log("Config url is overrided to: " + config_url)
+  }
+
+  try {
+    config = await fetch(config_url).then(res => {
+                        if (!res.ok) {
+                          throw new Error("Can not fetch config")
+                        }
+                        return res.json()
+                    })
+
+    console.log("Config fetch successful: " + JSON.stringify(config))
+  } catch {
+    console.log("Config fetch failed, fallback to default: " + local_default_config)
+    if (fs.existsSync(local_default_config)) {
+      config = require(local_default_config)
+    } else {
+      console.log(local_default_config + " Does not exists, no way to load config.")
+      app.exit(0)
+    }
+  }
+
+  console.log("Final config: " + JSON.stringify(config))
+}
 
 async function createWindow () {
   const win = new BrowserWindow({
@@ -8,25 +46,12 @@ async function createWindow () {
     icon: 'images/icon.png'
   })
 
-  var config;
-  try {
-    config = await fetch('https://bangoc.github.io/subpot/config.json')
-                            .then(res => {
-                        if (!res.ok) {
-                          throw new Error("Lỗi tải dữ liệu")
-                        }
-                        return res.json()
-                    })
-
-    console.log("Fetch success config: " + JSON.stringify(config))
-  } catch {
-    console.log("Fetch failed, fallback to default!")
-    config = require('./config-default.json')
+  if (typeof config != 'undefined' && 'home' in config) {
+    win.loadURL(config['home'])
+  } else {
+    console.log("home parameter is not set, can not start due to unknown home.")
+    return 1
   }
-
-  console.log("Final config: " + JSON.stringify(config))
-
-  win.loadURL(config['home'])
 
   win.webContents.on('will-navigate', function (e, url) {
     console.log("Will navigate: " + url)
@@ -94,9 +119,15 @@ menu.append(new MenuItem({
 
 Menu.setApplicationMenu(menu)
 
+async function doStart() {
+  await loadConfig()
+
+  await createWindow()
+}
+
 app.whenReady().then(() => {
 
-  createWindow()
+  doStart()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
